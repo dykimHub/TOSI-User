604fc97601b6fd6802d69b73dc6e7a2797cd1837
김다윤 <dy.jo.kim@gmail.com>
2024-10-17 00:01:03 +0900

    Fix: Redis CRUD Respository 추가 후 캐시 조회

--- user/src/main/java/com/tosi/user/dto/TaleDto.java
+++ user/src/main/java/com/tosi/user/dto/TaleDto.java
@@ -3,12 +3,17 @@ package com.tosi.user.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.List;
 
+@RedisHash("TaleDto") 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TaleDto {
+
+    @Id // Redis 고유 식별자
     private long taleId;
     private String title;
     private String thumbnailS3Key;
