e57c01e1e7837596c40c8de72729aed7cf226728
김다윤 <dy.jo.kim@gmail.com>
2024-10-16 23:35:48 +0900

    Fix: 즐겨찾는 동화 캐싱 수정

--- user/src/main/java/com/tosi/user/service/FavoriteServiceImpl.java
+++ user/src/main/java/com/tosi/user/service/FavoriteServiceImpl.java
@@ -26,13 +26,11 @@ public class FavoriteServiceImpl implements FavoriteService {
     /**
      * 회원 아이디와 동화 아이디를 이용해 해당 동화를 즐겨찾기에 추가합니다.
      * 이미 즐겨찾기 목록에 있는 동화라면 추가하지 않습니다.
-     * 회원(#회원번호)의 동화 즐겨찾기 목록을 갱신하기 위해 캐시를 비웁니다.
      *
      * @param userId 회원 번호
      * @param taleId 동화 번호
      * @return 즐겨찾기에 성공하면 SuccessResponse 객체 반환
      */
-    @CacheEvict(value = "favoriteListCache", key = "#userId")
     @Transactional
     @Override
     public SuccessResponse addFavoriteTale(Long userId, Long taleId) {
@@ -56,13 +54,11 @@ public class FavoriteServiceImpl implements FavoriteService {
 
     /**
      * 해당 회원의 동화 즐겨찾기 목록을 반환합니다.
-     * 동화 즐겨찾기 목록(#회원-즐겨찾기 페이지번호)을 캐시에 등록합니다.
      *
      * @param userId   회원 번호
      * @param pageable 페이지 번호, 페이지 크기, 정렬 기준 및 방향을 담고 있는 Pageable 객체
      * @return TaleDto 객체 리스트를 감싼 TaleDtos 객체
      */
-    @Cacheable(value = "favoriteListCache", key = "#userId + '-' + #pageable.pageNumber")
     @Override
     public TaleDto.TaleDtos findFavoriteTales(Long userId, Pageable pageable) {
         Page<Long> favoriteTaleIds = favoriteRepository.findByTaleIdsByUserId(userId, pageable);
@@ -88,13 +84,11 @@ public class FavoriteServiceImpl implements FavoriteService {
 
     /**
      * 해당 동화를 즐겨찾기 목록에서 삭제합니다.
-     * 회원(#회원 번호)의 동화 즐겨찾기 목록을 갱신하기 위해 캐시를 비웁니다.
      *
      * @param userId 회원 번호
      * @param taleId 동화 번호
      * @return 삭제가 완료되면 SuccessResponse 객체를 반환
      */
-    @CacheEvict(value = "favoriteListCache", key = "#userId")
     @Transactional
     @Override
     public SuccessResponse deleteFavoriteTale(Long userId, Long taleId) {
